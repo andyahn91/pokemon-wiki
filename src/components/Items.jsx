7224e3f4e81e818490e0c4cc1cdacc54b1c3ee06
Andy Ahn <andyahn91@gmail.com>
2019-02-17 23:38:27 -0800

    class to functional component

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
 
-// const Items = (props) => {
-//   return (
-//     <div className="card" style={{ width: "18rem" }}>
+const Items = (props) => {
+  const { imageURL, name, description} = props.pokemon;
+  const questinoMark = 'https://static.thenounproject.com/png/22161-200.png'
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src={imageURL === undefined ? questinoMark : imageURL}
+        className="card-img-top p-2 "
+      />
+      <div className="card-body bg-light">
+        <h5 className="card-title text-center">{name === undefined ? 'Loading...' : name}</h5>
+        <p className="card-text">{description === undefined ? 'Loading...' : description}</p>
+        <div className="text-right">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            onClick={props.onNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// class Items extends Component {
+//   constructor(props){
+//     super(props);
+//   }
+
+//   componentDidMount(){
+//     console.log('Item componentDidMount!');
+//   }
+
+//   render() {
+//     console.log('Items Component Rendered!');
+//     console.log('this.props.pokemon', this.props.pokemon)
+
+//     return (
+//       <div className="card" style={{ width: "18rem" }}>
 //         <img
 //           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
 //           className="card-img-top p-2 "
 //         />
 //         <div className="card-body bg-light">
-//           <h5 className="card-title text-center">{props.name}</h5>
+//           <h5 className="card-title text-center">{this.props.pokemon === undefined ? 'Loading...' : this.props.pokemon.name}</h5>
 //           <p className="card-text">
 //             Some quick example text to build on the card title and make up the
 //             bulk of the card's content.
@@ -16,55 +54,16 @@ import React, { Component } from "react";
 //           <div className="text-right">
 //             <button
 //               type="button"
-//               onMouseEnter={() => "btn btn-danger"}
-//               className="btn btn-outline-danger"
+//               onMouseEnter={() => "btn btn-info"}
+//               className="btn btn-outline-info"
 //             >
-//               Delete
+//               Next
 //             </button>
 //           </div>
 //         </div>
 //       </div>
-//   );
+//     );
+//   }
 // }
-
-class Items extends Component {
-  constructor(props){
-    super(props);
-    this.name = props.pokemon.name
-  }
-
-  componentDidMount(){
-    console.log('Item componentDidMount!');
-  }
-
-  render() {
-    console.log('Items Component Rendered!');
-
-    return (
-      <div className="card" style={{ width: "18rem" }}>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-          className="card-img-top p-2 "
-        />
-        <div className="card-body bg-light">
-          <h5 className="card-title text-center">{this.name === undefined ? 'Loading' : this.name}</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <div className="text-right">
-            <button
-              type="button"
-              onMouseEnter={() => "btn btn-info"}
-              className="btn btn-outline-info"
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default Items;
