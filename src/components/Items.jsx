1b9bc04a7e6ef757c669287ed66e09cea1937f19
Andy Ahn <andyahn91@gmail.com>
2019-02-19 19:30:42 -0800

    function load added to make code clearner, unnecessary comments are deleted

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,69 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 
 const Items = (props) => {
-  const { imageURL, name, description} = props.pokemon;
-  const questinoMark = 'https://static.thenounproject.com/png/22161-200.png'
+  const { imageURL, name, description } = props.pokemon;
+  const questionMark = 'https://static.thenounproject.com/png/22161-200.png';
+
+  /**
+   * Returns its argument. If nothing is passed as an argument, it returns a string 'Loading...'
+   * @param {string} variable
+   */
+  const load = (variable) => {
+    return variable === undefined ? 'Loading...' : variable;
+  };
+
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card mx-3" style={{ width: '18rem', height: '34rem' }}>
       <img
-        src={imageURL === undefined ? questinoMark : imageURL}
-        className="card-img-top p-2 "
+        src={imageURL === undefined ? questionMark : imageURL}
+        className="card-img-top p-2"
+        alt=""
       />
       <div className="card-body bg-light">
-        <h5 className="card-title text-center">{name === undefined ? 'Loading...' : name}</h5>
-        <p className="card-text">{description === undefined ? 'Loading...' : description}</p>
-        <div className="text-right">
+        <h5 className="card-title text-center">{load(name)}</h5>
+        <p className="card-text overflow-auto">{load(description)}</p>
+        {/* <div className="text-right">
           <button
             type="button"
-            className="btn btn-outline-success"
-            onClick={props.onNext}
+            className="btn btn-outline-danger btn-sm"
+            onClick={props.onDelete}
           >
-            Next
+            Delete
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
-}
-
-// class Items extends Component {
-//   constructor(props){
-//     super(props);
-//   }
-
-//   componentDidMount(){
-//     console.log('Item componentDidMount!');
-//   }
-
-//   render() {
-//     console.log('Items Component Rendered!');
-//     console.log('this.props.pokemon', this.props.pokemon)
-
-//     return (
-//       <div className="card" style={{ width: "18rem" }}>
-//         <img
-//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-//           className="card-img-top p-2 "
-//         />
-//         <div className="card-body bg-light">
-//           <h5 className="card-title text-center">{this.props.pokemon === undefined ? 'Loading...' : this.props.pokemon.name}</h5>
-//           <p className="card-text">
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </p>
-//           <div className="text-right">
-//             <button
-//               type="button"
-//               onMouseEnter={() => "btn btn-info"}
-//               className="btn btn-outline-info"
-//             >
-//               Next
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+};
 
 export default Items;
