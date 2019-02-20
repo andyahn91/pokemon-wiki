c23090e418e11a6728c075004cfb1834371f6a1f
Andy Ahn <andyahn91@gmail.com>
2019-02-19 19:27:37 -0800

    Left and Right buttons are now being imported as functional components, deleted unnecessary console.logs, and organized inside of return statement

--- src/App.js
+++ src/App.js
@@ -1,87 +1,92 @@
-import React, { Component } from 'react';
-import Items from './components/Items.jsx';
-import NavBar from './components/NavBar.jsx';
-import Add from './components/Add.jsx';
-require('./App.css');
+import React, { Component } from "react";
+import Items from "./components/Items.jsx";
+import NavBar from "./components/NavBar.jsx";
+import Add from "./components/Add.jsx";
+import LeftItem from "./components/LeftItem.jsx";
+import RightItem from "./components/RightItem.jsx";
+require("./App.css");
 
 class App extends Component {
   state = {
     pokemons: [],
     current: 0,
     pokemon: {
-      imageURL: '',
-      name: '',
-      description: '',
+      imageURL: "",
+      name: "",
+      description: ""
     }
-  }
+  };
 
-  async componentDidMount(){
-    console.log('App componentDidMount!');
-    let data = await fetch('http://localhost:3001/')
+  //
+  async componentDidMount() {
+    let data = await fetch("http://localhost:3001/");
     let result = await data.json();
 
     this.setState({
       pokemons: [...result],
       pokemon: result[this.state.current]
-    })
-      // .then(res => res.json())
-      // .then(res => {
-      //   this.setState({
-      //     pokemons : [...res]
-      //   });
-      //   console.log('sfdsfdsfsddsf', this.state.pokemons[this.state.current])
-      //   console.log('App this.state.pokemon', this.state.pokemon);
-      // })
-      // .catch(err => console.log(err));
-      // this.setState({
-      //   pokemon : this.state.pokemons[this.state.current]
-      // })
-      // console.log(data.json())
-      // console.log(data)
+    });
   }
 
+  handlePrevious = () => {
+    let newCurrent = this.state.current + 1;
+
+    if (this.state.current <= this.state.pokemons.length - 1) {
+      this.setState({
+        current: 0,
+        pokemon: this.state.pokemons[0]
+      });
+    } else {
+      this.setState({
+        current: (this.state.current += 1),
+        pokemon: this.state.pokemons[newCurrent]
+      });
+    }
+  };
+
   handleNext = () => {
-    console.log('current', this.state.current)
     let newCurrent = this.state.current + 1;
 
     if (this.state.current >= this.state.pokemons.length - 1) {
       this.setState({
         current: 0,
         pokemon: this.state.pokemons[0]
-      })
-    }else {
+      });
+    } else {
       this.setState({
-        current: this.state.current += 1,
+        current: (this.state.current += 1),
         pokemon: this.state.pokemons[newCurrent]
-      })
-
+      });
     }
+  };
 
-    
+  handleDelete = () => {
+
   }
 
   render() {
-    console.log('App Component Rendered!');
     return (
-      <div className="container-fluid h-100">
-        <NavBar id="navBar"/>
-        <Add />
-        <div class="row justify-content-center h-100">
-          <div class="col-sm">
-          </div>
-          <div class="col-sm">
-            <Items id="items" pokemon={this.state.pokemon} onNext={this.handleNext}/>
-          </div>
-          <div class="col-sm col3 vcenter">
-            <button
-              type="button"
-              className="btn btn-outline-info"
-            >
-              Neeeext
-            </button>
+      <React.Fragment>
+        <NavBar />
+        <div className="container-fluid p-3">
+          <Add />
+          <div className="row align-items-center">
+            <div className="col-sm d-flex flex-row-reverse">
+              <LeftItem onClick={this.handlePrevious} />
+            </div>
+            <div className="col-sm d-flex justify-content-center">
+              <Items
+                pokemon={this.state.pokemon}
+                onNext={this.handleNext}
+                onDelete={this.handleDelete}
+              />
+            </div>
+            <div className="col-sm d-flex flex-row">
+            <RightItem onClick={this.handleNext} />
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
