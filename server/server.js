adf0052b52440d96cf3b7973f6ba8cd47b7ef585
Andy Ahn <andyahn91@gmail.com>
2019-02-20 07:41:14 -0800

    organized codes to make it look clean and deleted a middleware in get request to use itemController.getItem

--- server/server.js
+++ server/server.js
@@ -6,31 +6,25 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const itemController = require('./controllers/itemController')
+const itemController = require('./controllers/itemController');
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true }, () => { console.log('DATABASE CONNECTED')});
+// connects to the database using address stored in .env file
+mongoose.connect(process.env.DB, { useNewUrlParser: true }, () => { console.log('DATABASE CONNECTED'); });
 
 const app = express();
 
+// parses incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors())
+// handle CORS error
+app.use(cors());
 
-const Item = require('./models/ItemModel');
-
-app.get('/', (req, res) => {
-  Item.find()
-    .then(data => res.json(data))
-})
+app.get('/', itemController.getItem);
 
 app.post('/add', itemController.postItem);
 
 const PORT = 3001;
 
 app.listen(PORT, (error) => {
-  if(error) {
-    console.log(error);
-  } else {
-    console.log(`LISTENING ON PORT ${PORT}`);
-  }
+  console.log(error || `LISTENING ON PORT ${PORT}`);
 });
