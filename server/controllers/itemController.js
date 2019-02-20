df934c727eec62dd6149721e35e8265e33dd97a9
Andy Ahn <andyahn91@gmail.com>
2019-02-19 23:50:31 -0800

    calm down eslint

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -3,29 +3,29 @@ const Item = require('./../models/ItemModel.js');
 module.exports = {
   getItem(req, res) {
     Item.find()
-      .then(res => console.log(res))
+      .then(res => console.log(res));
   },
   postItem(req, res, next) {
-    if(req.body.name != null && req.body.name != "" &&
-      req.body.description != null && req.body.description != ""){
+    if (req.body.name !== null && req.body.name !== '' &&
+      req.body.description !== null && req.body.description !== '') {
       Item.create({
         imageURL: req.body.imageURL,
         name: req.body.name,
         description: req.body.description,
       })
-      .then(message => res.json(message))
-      .catch(err => {
-        res.status(500);
-        res.json(err);
-      });
+        .then(message => res.json(message))
+        .catch((err) => {
+          res.status(500);
+          res.json(err);
+        });
     } else {
       res.status(400);
-      res.json({message:"Please pass name and description"});
+      res.json({ message: 'Please pass name and description' });
     }
   },
   deleteItem(req, res) {
     Item.deleteOne({
-      _id: req.body._id
+      _id: req.body._id,
     })
       .then(deletedItem => res.json(deletedItem))
       .then(res => console.log('deleted'))
